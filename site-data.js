f34414780af8a322bb9c32debb75647d48059ff4
AntF-web <flamenta@hotmail.com>
2026-08-08 05:19:51 +0200

    Update site-data.js

--- site-data.js
+++ site-data.js
@@ -24,8 +24,8 @@ window.RED_ANT_SITE_DATA = {
       "memory": {
         "video": "https://www.youtube.com/watch?v=BzAZ7v1z-8o&t=3s",
         "image": "https://github.com/AntF-web/MusicStory/blob/main/images/empire-windrush-packed-with-west-indian-immigrants-on-arrival-at-the-port-of-tilbury-on-the-river-thames-on-22-june-1948-copyright-contraband-collection-alamy-images-766x510.jpeg?raw=true",
-        "text": "The HMT Empire Windrush is the historic ship that famously arrived at Tilbury Docks near London on June 22, 1948, carrying 1,027 passengers and two stowaways. Boarding primarily in Kingston, Jamaica, the passengers answered the British government's call to help rebuild post-war Britain, making this voyage the enduring symbol of modern multicultural Britain and the origin of the term \"Windrush Generation\". Many descendants of the Windrush generation have become musicians who created the music and influences of today.",
-        "textFr": "Le HMT Empire Windrush est le navire historique qui, comme chacun sait, est arrivé aux docks de Tilbury, près de Londres, le 22 juin 1948 avec à son bord 1 027 passagers et deux passagers clandestins. Embarqués principalement à Kingston, en Jamaïque, ces passagers avaient répondu à l'appel du gouvernement britannique visant à reconstruire le pays après la guerre ; ce voyage est ainsi devenu le symbole durable d'une Grande-Bretagne multiculturelle moderne et a donné naissance à l'expression « génération Windrush ». De nombreux descendants de cette génération sont devenus des musiciens à l'origine des musiques et des influences actuelles."
+        "text": "The HMT Empire Windrush is the historic ship that famously arrived at Tilbury Docks near London on June 22, 1948, carrying 1,027 passengers and two stowaways. Boarding primarily in Kingston, Jamaica, the passengers answered the British government's call to help rebuild post-war Britain, making this voyage the enduring symbol of modern multicultural Britain and the origin of the term \"Windrush Generation\". \nMany descendants of the Windrush generation have become musicians who created the music and influences of today.",
+        "textFr": "Le HMT Empire Windrush est le navire historique qui, comme chacun sait, est arrivé aux docks de Tilbury, près de Londres, le 22 juin 1948 avec à son bord 1 027 passagers et deux passagers clandestins. Embarqués principalement à Kingston, en Jamaïque, ces passagers avaient répondu à l'appel du gouvernement britannique visant à reconstruire le pays après la guerre ; ce voyage est ainsi devenu le symbole durable d'une Grande-Bretagne multiculturelle moderne et a donné naissance à l'expression « génération Windrush ». \nDe nombreux descendants de cette génération sont devenus des musiciens à l'origine des musiques et des influences actuelles."
       },
       "note": "Jamaican & UK are the roots of a new world in music.",
       "noteFr": "Le lien entre la Jamaïque et le Royaume-Uni ont changé la musique pour toujours.",
@@ -40,7 +40,8 @@ window.RED_ANT_SITE_DATA = {
       "meta": "Jamaican Mento / Calypso",
       "memory": {
         "video": "https://www.youtube.com/watch?v=pUPmG-pT9C0",
-        "text": "Calypso roots."
+        "text": "Calypso roots.",
+        "textFr": "Les racines du Calypso."
       }
     },
     {
